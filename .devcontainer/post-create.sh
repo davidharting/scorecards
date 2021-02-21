4665c00be391c119d61f1e7ca8227e79587c987f
David Harting <david.e.harting@gmail.com>
2021-02-21 19:44:24 +0000

    Some more basic setup
    Now I have a functioning dev environment

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# bundle install
-# bundle exec rails db:create
-# bundle exec rails db:migrate
-# bundle exec rails db:test:prepare
+echo "Installing dependencies from Gemfile"
+bundle install
+echo "Creating development database"
+bundle exec rails db:create
+echo "Migrating development database"
+bundle exec rails db:migrate
+echo "Preparing test database"
+bundle exec rails db:test:prepare
+
+echo "post-create script is done. Your development environment is ready!"
--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Workspace
+module KeepScore
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
-    config.application_name = Rails.application.class.module_parent_name
+    config.application_name = "Keep Score"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
