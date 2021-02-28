64c0373bd0158ceb6b236cbc8594b738aba4186e
David Harting <david.e.harting@gmail.com>
2021-02-28 20:27:52 +0000

    Add player model

---
+++ app/models/player.rb
@@ -0,0 +1,5 @@
+class Player < ApplicationRecord
+  belongs_to :scorecard
+  validates :name, presence: true
+  validates :name, length: { minimum: 1, maximum: 60 }
+end
--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -1,5 +1,5 @@
 class Scorecard < ApplicationRecord
   belongs_to :user
-
+  has_many :players
   validates :name, length: { minimum: 1, maximum: 280 }
 end
