a8f0c6ecbb4bda4070fb31e930ff8772ad316c18
David Harting <david.e.harting@gmail.com>
2021-03-08 21:56:14 +0000

    refactor input component to support numbers
    I don't really like this - I would rather explicitly separate text input from number input, or at least pass a param. But this will work for now.

---
+++ app/components/forms/input_component.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class Forms::InputComponent < ViewComponent::Base
+  def initialize(form:, attribute:, help:, label: nil)
+    @form = form
+    @attribute = attribute
+    @label = label
+    @help = help
+    @input_cx = input_cx
+    @has_error = has_error
+    @error_message = error_message
+    @field_type = field_type
+
+    pry
+  end
+
+  private
+
+  def has_error
+    @form.object.errors.has_key?(@attribute)
+  end
+
+  def input_cx
+    has_error ? 'form-control is-invalid' : 'form-control'
+  end
+
+  def error_message
+    if has_error
+      @form.object.errors.full_messages_for(@attribute).join(' ')
+    else
+      nil
+    end
+  end
+
+  # Return :text_field or :number_field
+  def field_type
+    type = @form.object.class.columns_hash[@attribute.to_s].type
+    return :text_field if type == :string
+    return :number_field
+  end
+end
