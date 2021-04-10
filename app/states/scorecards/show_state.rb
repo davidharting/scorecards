78f7cd6e4760c5dfea99eab0ab9ed632d0135daf
David Harting <david.e.harting@gmail.com>
2021-04-10 20:54:56 +0000

    remove puts

--- app/states/scorecards/show_state.rb
+++ app/states/scorecards/show_state.rb
@@ -1,8 +1,6 @@
 module Scorecards
   class ShowState
     def initialize(user:, scorecard_id:)
-      puts("!!!!!!!!!!!!!\n\n#{user}")
-      puts "initialize state #{scorecard_id} "
       @scorecard = user.scorecards.find(scorecard_id)
       @players = Scorecards.ordered_players(@scorecard)
       @rounds = @scorecard.rounds.order(number: :asc)
