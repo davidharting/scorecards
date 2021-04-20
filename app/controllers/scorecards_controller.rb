d3775a96dfe12176154214fe7d78f77d3d13f0bf
David Harting <david.e.harting@gmail.com>
2021-04-19 20:28:26 -0400

    Add / remove players when creating a game (#4)
    Went ahead and included a walkthrough of the scorecard as well since I don't think I've captured that yet. But the change here is at the beginning - you can now Add / Remove players when setting up a scorecard.
    
    https://user-images.githubusercontent.com/6146772/115319358-a9e76500-a14d-11eb-9cdb-ec1685bb3224.mp4

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -8,29 +8,55 @@ class ScorecardsController < ApplicationController
 
   def new
     @scorecard = Scorecard.new
-    4.times { @scorecard.players.build }
+    @scorecard.players.build
   end
 
   def create
     @scorecard = current_user.scorecards.new(scorecard_params)
-    @scorecard.players.each { |p| p.destroy if p.name.nil? or p.name.empty? }
-
-    if @scorecard.save
-      flash[:notice] = 'Successfully created scorecard'
-      redirect_to root_path
-    else
-      flash[:alert] = 'Failed to create scorecard.'
+    if add_player?
+      @scorecard.players.build
       render :new
+    elsif remove_player?
+      players = @scorecard.players.to_a
+      @scorecard.players.delete_all
+
+      player_to_remove_index = remove_player?
+      players.delete_at(player_to_remove_index)
+
+      players.each { |player| @scorecard.players.build(player.as_json) }
+
+      render :new
+    else
+      @scorecard.players.each { |p| p.destroy if p.name.nil? or p.name.empty? }
+
+      if @scorecard.save
+        flash[:notice] = 'Successfully created scorecard'
+        redirect_to root_path
+      else
+        flash[:alert] = 'Failed to create scorecard.'
+        render :new
+      end
     end
   end
 
   private
 
-  def ordered_players(scorecard)
-    scorecard.players.order(id: :asc)
-  end
-
   def scorecard_params
     params.require(:scorecard).permit(:name, players_attributes: [:name])
+  end
+
+  def add_player?
+    btn_value = params[:button]
+    btn_value == 'add_player' ? true : false
+  end
+
+  def remove_player?
+    btn_value = params[:button]
+    if btn_value.start_with?('remove_player=')
+      split = btn_value.split '='
+      split[1].to_i
+    else
+      false
+    end
   end
 end
